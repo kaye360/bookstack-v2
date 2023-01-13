752cc7ec6836dd414470298de869913f2599b9f2
joshkaye88 <jkaye@shaw.ca>
2023-01-13 13:08:58 -0800

    refactor index.php and move routes out of the file

--- api/index.php
+++ api/index.php
@@ -14,7 +14,7 @@
 require_once './config.php';
 
 /**
- * Header
+ * Headers
  */
 header("Access-Control-Allow-Origin: " . ALLOW_ORIGIN);
 header("Content-Type: application/json; charset=UTF-8");
@@ -35,58 +35,8 @@ require_once './models/Book.php';
 require_once './models/Comment.php';
 
 /**
- * Init Router
+ * Routes
+ * $route->json() renders route data to JSON
  */
-$route = new Router();
-$route->baseURL = BASE_URL;
-
-/**
- * User Routes
- */
-$route->get('/users', ['User', 'get_all']);
-$route->get('/user/:id', ['User', 'get_single', 'id']);
-$route->post('/user', ['User', 'create']);
-$route->post('/user/login', ['User', 'login']);
-$route->post('/user/logout', ['User', 'logout']);
-$route->put('/user/:id', ['User', 'edit', 'id']);
-$route->delete('/user/:id', ['User', 'destroy', 'id']);
-
-/**
- * User Notification Routes
- */
-$route->get('/notifications/new/:id', ['User', 'get_new_notifications', 'id']);
-$route->get('/notifications/old/:id', ['User', 'get_old_notifications', 'id']);
-$route->put('/notifications/add', ['User', 'add_new_notification']);
-$route->put('/notifications/clear_new', ['User', 'clear_new_notifications']);
-
-/**
- * Book Routes
- */
-$route->post('/book', ['Book', 'create']);
-$route->get('/book/:id', ['Book', 'get_single', 'id']);
-$route->get('/books/:id', ['Book', 'get_all', 'id']);
-$route->put('/book/:id', ['Book', 'edit', 'id']);
-$route->put('/book/like', ['Book', 'like']);
-$route->put('/book/read', ['Book', 'toggle_read_status']);
-$route->put('/book/unlike', ['Book', 'unlike']);
-$route->delete('/book/:id', ['Book', 'destroy', 'id']);
-
-/**
- * Comment Routes
- */
-$route->post('/comment', ['Comment', 'create']);
-$route->get('/comment/:id', ['Comment', 'get_single', 'id']);
-$route->get('/comments/:id', ['Comment', 'get_all', 'id']);
-$route->put('/comment/:id', ['Comment', 'edit', 'id']);
-$route->delete('/comment/:id', ['Comment', 'destroy', 'id']);
-
-/**
- * Dev Routes
- * For generating fake data for testing purposes.
- */
-$route->get('/user-reset', ['User', 'reset']);
-$route->get('/book-reset', ['Book', 'reset']);
-$route->get('/comment-reset', ['Comment', 'reset']);
-
-// Display route data as JSON
+require_once './routes/routes.php';
 $route->json();
