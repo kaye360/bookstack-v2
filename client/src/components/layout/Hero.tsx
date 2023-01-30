726e4670ea922508aa6ba2c82cf4f5e964e032fc
joshkaye88 <jkaye@shaw.ca>
2023-01-29 23:08:24 -0800

    update hero and homepage. add poppins font

--- client/src/components/layout/Hero.tsx
+++ client/src/components/layout/Hero.tsx
@@ -14,12 +14,12 @@ export default function Hero() {
 
             <Header />
 
-            <div className="relative max-w-xl z-40 text-2xl px-4 flex flex-col gap-12">
+            <div className="relative max-w-xl z-40 pl-12 py-12 flex flex-col gap-6">
                 <h1 className="text-3xl font-bold leading-normal tracking-wide">
                     A Better Way to Manage Your Personal Book Collection
                 </h1>
 
-                <p className='leading-normal tracking-wide'>
+                <p className='leading-normal tracking-wide max-w-[50ch]'>
                     Organize and Track Your Personal Book Collection with Ease. Simply enter a book ISBN and go...
                 </p>
 
@@ -46,8 +46,12 @@ export default function Hero() {
                             </Link>
                         </div>
                 }
+
+                <div className="absolute top-[1rem] left-0 bottom-[0rem] z-[-1] w-[20vw] min-w-[200px] rounded-xl bg-primary-700 bg-opacity-25"></div>
+
             </div>
 
+            {/* Hero BG */}
             <div className="absolute inset-0 z-20 pl-[25vw] md:pl-56 overflow-hidden">
                 <img src={ hero } className="w-full h-full object-cover" />
             </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,30 +1,49 @@
 
 import Hero from "../components/layout/Hero";
 import Explore from "../components/library/Explore";
+import booksScattered from "../assets/img/books-scattered.png"
+import booksAngled from "../assets/img/books-angled.png"
 
 export default function Home() {
 
     
     return(
     <div className="flex flex-col gap-12">
-        <Hero></Hero>
+        
+        <Hero />
 
-        <div className="grid md:grid-cols-2 gap-4">
+        <div className="grid md:grid-cols-2 gap-x-2 gap-y-12 items-center">
 
-            <div className="rounded border border-slate-300 bg-slate-100 aspect-video">
-                Screenshot
+            <div className="">
+                <img src={booksAngled} className="w-4/5" />
             </div>
 
-            <div className="aspect-video py-12">
-                About the App. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores expedita sed repellat quas quae debitis unde corrupti aperiam nemo nesciunt?
+            <div className="">
+
+                <h2 className="text-3xl mb-8">
+                    Stay Organized
+                </h2>
+
+                <p>
+                    About the App. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores expedita sed repellat quas quae debitis unde corrupti aperiam nemo nesciunt?
+                </p>
+
             </div>
 
-            <div className="aspect-video">
-                About the App . Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, impedit! Voluptate, incidunt molestias iste amet consequatur dolore enim quibusdam. Minus perspiciatis labore fugiat deleniti provident, asperiores laboriosam repudiandae consequuntur incidunt!
+            <div className="">
+
+                <h2 className="text-3xl mb-8">
+                    Share your collection
+                </h2>
+
+                <p>
+                    About the App . Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, impedit! Voluptate, incidunt molestias iste amet consequatur dolore enim quibusdam. Minus perspiciatis labore fugiat deleniti provident, asperiores laboriosam repudiandae consequuntur incidunt!
+                </p>
+
             </div>
 
-            <div className="rounded border border-slate-300 bg-slate-100 aspect-video">
-                Screenshot
+            <div className="">
+                <img src={booksScattered} className="w-4/5 ml-auto"  />
             </div>
 
         </div>
