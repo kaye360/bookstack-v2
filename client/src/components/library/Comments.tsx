4ce90661b03b835e30ddca7f3458635cdf73b58b
joshkaye88 <jkaye@shaw.ca>
2023-02-03 21:00:00 -0800

    refactor comments components, make book page responsive

---
+++ client/src/components/library/Comments.tsx
@@ -0,0 +1,117 @@
+import { useState, useContext } from "react"
+import { useQueryClient, useQuery } from "react-query"
+import { Link } from "react-router-dom"
+import { UserContext } from "../../App"
+import { API_BASE_URL } from "../../config"
+import httpReq from "../../utils/httpReq"
+
+export function Comments({isLoading, isError, book}) {
+
+    if(isLoading) {
+        return <div>Comments are loading</div>
+    }
+
+    if(isError) {
+        return <div>Error Loading Comments</div>
+    }
+
+    if(book.comments.success === false) {
+        return <div>No comments were found on this book. Be the first!</div>
+    }
+
+    return (
+        <ul className="flex flex-col gap-8 text-left" id="comments">
+            { 
+            book.comments.map( comment => {
+                return(
+                    <li key={comment.id}>
+                        <h3 className="w-fit px-4 py-2 rounded-2xl font-bold text-xl bg-primary-900">
+                            <Link to={`/user/${comment.username}`}>
+                                {comment.username}
+                            </Link>
+                        </h3>
+                        <p className="m-[1rem_0_1rem_1rem] px-4 border-l-4 border-primary-600">
+                            {comment.comment}
+                        </p>
+                    </li>
+            )})
+            }
+        </ul>
+    )
+}
+
+
+
+export function CommentForm({username, userID, bookID, bookTitle, updateComments}) {
+
+    const [userComment, setUserComment] = useState('')
+    const queryClient = useQueryClient()
+    const { isLoggedIn } = useContext(UserContext)
+
+    async function postComment() {
+        
+
+        const comment = {
+            username : username,
+            user_id : userID,   
+            book_id : bookID,
+            book_title : bookTitle,
+            comment : userComment
+        }
+
+        const res = await httpReq.post(API_BASE_URL + '/comment', comment)
+
+        if (!res.success) throw new Error(res.message)
+        
+        updateComments()
+
+        setTimeout( () => {
+            queryClient.resetQueries('submitComment', { exact : true } )
+        }, 2000)
+
+        return res
+    }
+
+    const {data, error, isError, isLoading, refetch, isSuccess} = useQuery('submitComment', postComment, { enabled:false })
+
+    async function handleSubmit(e: any) {
+        e.preventDefault()
+        refetch()
+    }
+
+    return(
+        <form onSubmit={handleSubmit} >
+            <div className="flex flex-col gap-4 text-left max-w-lg my-4 p-4 rounded-md bg-primary-600">
+
+                <h4 className="font-bold">
+                    { isLoggedIn 
+                        ? <>
+                            Commenting as: { username } &nbsp;
+                            <span className="text-sm font-normal">
+                                ({userComment.length}/200 characters)
+                            </span>
+                        </>
+                        : <span className="inline-block ml-2 font-normal">Please sign in to post a comment</span>
+                    }
+                </h4>
+
+                { isLoggedIn && <>
+                    <textarea 
+                        className="resize-none rounded p-2 w-full h-32 bg-primary-300 text-primary-800"
+                        onChange={ (e) => setUserComment(e.target.value) }
+                        value={userComment}
+                    ></textarea>
+
+                    <div className="flex items-center gap-4">
+                        <input type="submit" value="Post Comment" className="rounded-md px-4 py-2 bg-primary-200 text-primary-900 font-bold w-fit cursor-pointer" />
+                        { isSuccess && 'Comment Posted!'}
+                        { isLoading && 'Please wait...'}
+                        { isError && error.message }
+                    </div>
+                </>
+                }
+
+            </div>
+        </form>
+    )
+}
--- client/src/pages/Book.tsx
+++ client/src/pages/Book.tsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 
 import { API_BASE_URL } from "../config";
 import { UserContext } from "../App";
@@ -16,6 +16,8 @@ import iconLike from "../assets/img/icon-like.png"
 import iconLiked from "../assets/img/icon-liked.png"
 import iconChat from "../assets/img/icon-chat.png"
 import iconDelete from "../assets/img/icon-delete.png"
+import { Comments, CommentForm } from "../components/library/Comments";
+import React from "react";
 
 
 export default function Book() {
@@ -54,7 +56,7 @@ export default function Book() {
 
         <BackBtn />
 
-        <div className="relative grid grid-cols-[3fr_1.5fr] gap-8">
+        <div className="relative grid sm:grid-cols-[3fr_1.5fr] gap-16 sm:gap-8">
 
             {/* Header blur Background */}
             <img 
@@ -62,7 +64,7 @@ export default function Book() {
                 className="absolute top-0 left-0 right-0 z-10 object-cover object-center w-full h-[50vh] blur-lg opacity-20"
             />
 
-            {/* Book Info Column */}
+            {/* Book Info Header */}
             <div className="relative z-20 flex flex-col gap-8">
 
                 <h1 className="text-3xl">
@@ -71,11 +73,11 @@ export default function Book() {
 
                 <div className="border border-primary-400"></div>
 
-                <p className="font-semibold">
-                    {googleQuery.data.items[0].volumeInfo.subtitle}
-                </p>
-
                 <div>
+                    <p className="font-semibold">
+                        {googleQuery.data.items[0].volumeInfo.subtitle}
+                    </p>
+
                     <p>
                         By: {bookQuery.data.author}
                     </p>
@@ -89,15 +91,9 @@ export default function Book() {
                         Google Rating: {googleQuery.data.items[0].volumeInfo.averageRating}/5
                     </p>
                 </div>
-
-                { description.map( (paragraph, index) => (
-                    <p className="text-lg" key={index}>{paragraph}</p>
-                ))}
-
-
             </div>
 
-            {/* Book Cover Column */}
+            {/* Book Cover */}
             <div className="relative z-20">
                 <img 
                     src={bookQuery.data.cover_url} 
@@ -140,8 +136,22 @@ export default function Book() {
                 </div>
 
             </div>
+
+            {/* Book Description */}
+            <div>
+                { description.map( (paragraph, index) => (
+                    <React.Fragment key={index}>
+                        <h2 className="my-4 font-bold">Description</h2>
+                        <p className="text-lg" key={index}>
+                            {paragraph}
+                        </p>
+                    </React.Fragment>
+                ))}
+
+            </div>
         </div>
     
+
         <section className="" id="comments">
             <h2 className="my-4 py-4 text-2xl font-bold border-t border-slate-200">Comments</h2>
 
@@ -281,120 +291,5 @@ function DeleteModal({bookID, setShowModal}) {
                 <button onClick={ () => setShowModal(false)} >No, close this.</button>
             </>}
         </Modal>
-    )
-}
-
-
-
-
-
-function Comments({isLoading, isError, book}) {
-
-    if(isLoading) {
-        return <div>Comments are loading</div>
-    }
-
-    if(isError) {
-        return <div>Error Loading Comments</div>
-    }
-
-    if(book.comments.success === false) {
-        return <div>No comments were found on this book. Be the first!</div>
-    }
-
-    return (
-        <ul className="flex flex-col gap-8 text-left" id="comments">
-            { 
-            book.comments.map( comment => {
-                return(
-                    <li key={comment.id}>
-                        <h3 className="w-fit px-4 py-2 rounded-2xl font-bold text-xl bg-primary-900">
-                            <Link to={`/user/${comment.username}`}>
-                                {comment.username}
-                            </Link>
-                        </h3>
-                        <p className="m-[1rem_0_1rem_1rem] px-4 border-l-4 border-primary-600">
-                            {comment.comment}
-                        </p>
-                    </li>
-            )})
-            }
-        </ul>
-    )
-}
-
-
-
-function CommentForm({username, userID, bookID, bookTitle, updateComments}) {
-
-    const [userComment, setUserComment] = useState('')
-    const queryClient = useQueryClient()
-    const { isLoggedIn } = useContext(UserContext)
-
-    async function postComment() {
-        
-
-        const comment = {
-            username : username,
-            user_id : userID,   
-            book_id : bookID,
-            book_title : bookTitle,
-            comment : userComment
-        }
-
-        const res = await httpReq.post(API_BASE_URL + '/comment', comment)
-
-        if (!res.success) throw new Error(res.message)
-        
-        updateComments()
-
-        setTimeout( () => {
-            queryClient.resetQueries('submitComment', { exact : true } )
-        }, 2000)
-
-        return res
-    }
-
-    const {data, error, isError, isLoading, refetch, isSuccess} = useQuery('submitComment', postComment, { enabled:false })
-
-    async function handleSubmit(e: any) {
-        e.preventDefault()
-        refetch()
-    }
-
-    return(
-        <form onSubmit={handleSubmit} >
-            <div className="flex flex-col gap-4 text-left max-w-lg my-4 p-4 rounded-md bg-primary-600">
-
-                <h4 className="font-bold">
-                    { isLoggedIn 
-                        ? <>
-                            Commenting as: { username } &nbsp;
-                            <span className="text-sm font-normal">
-                                ({userComment.length}/200 characters)
-                            </span>
-                        </>
-                        : <span className="inline-block ml-2 font-normal">Please sign in to post a comment</span>
-                    }
-                </h4>
-
-                { isLoggedIn && <>
-                    <textarea 
-                        className="resize-none rounded p-2 w-full h-32 bg-primary-300 text-primary-800"
-                        onChange={ (e) => setUserComment(e.target.value) }
-                        value={userComment}
-                    ></textarea>
-
-                    <div className="flex items-center gap-4">
-                        <input type="submit" value="Post Comment" className="rounded-md px-4 py-2 bg-primary-200 text-primary-900 font-bold w-fit cursor-pointer" />
-                        { isSuccess && 'Comment Posted!'}
-                        { isLoading && 'Please wait...'}
-                        { isError && error.message }
-                    </div>
-                </>
-                }
-
-            </div>
-        </form>
     )
 }
