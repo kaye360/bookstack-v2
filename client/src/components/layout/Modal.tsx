ee35bd2976ee3ddd784294dfee1d675e665596e8
joshkaye88 <jkaye@shaw.ca>
2023-02-06 23:29:12 -0800

    fixed modal click exit bug

--- client/src/components/layout/Modal.tsx
+++ client/src/components/layout/Modal.tsx
@@ -5,11 +5,19 @@ interface IModalProps {
     children: ReactNode
 }
 
+/**
+ * @todo fix id type
+ */
+
 export default function Modal({setModalState, children} : IModalProps) {
 
         return <div 
             className="fixed inset-0 grid items-center px-3 bg-primary-900 z-[1000] bg-opacity-75"
-            onClick={() => setModalState(false)}
+            id="modal"
+            onClick={(e) => {
+                console.log(e.target)
+                if (e.target.id === 'modal') setModalState(false)}
+            }
         >
 
             <div className="relative z-[2000] rounded-xl flex flex-col gap-8 bg-primary-200 w-full max-w-xl mx-auto p-4 text-primary-800">
